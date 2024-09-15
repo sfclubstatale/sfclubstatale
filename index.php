e29427a541b6ec832bab83670cf878a5a4b09283
simonetriozzi <72708934+simonetriozzi@users.noreply.github.com>
2024-09-15 23:34:49 +0200

    Update and rename index.php to index.html

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Esegui un reindirizzamento a una nuova pagina
-header("Location: SF/home.html");
-exit(); // Assicura che il codice successivo non venga eseguito dopo il reindirizzamento
-?>
